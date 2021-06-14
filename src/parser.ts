fc0a0fe463d1ebe5d9a21a0af718c7ee601007b3
Lictor Guzman <lctrgzmn@gmail.com>
2021-06-13 21:04:22 -0500

    reworked for basic logical/arithmetic expressions

--- src/parser.ts
+++ src/parser.ts
@@ -1,174 +1,84 @@
-import { Lexer, Token, Type } from './lexer.ts';
+import { Lexer, Token, Type} from './lexer.ts';
 
-interface Expr {
-  type: Op | Type;
-  operator?: string,
-  left?: Expr | Token;
-  right?: Expr | Token;
-  value?: number | string;
+enum Rule {
+  Or, And, Equality, Compare, Term, Factor, Unary, Literal
 }
 
-// enum Punct {
-//   LParen, RParen, LCurly, RCurly, LSquare, RSquare, Vert, Slash, Semi, Comma
-// }
-
-enum Op {
-  Sequence, Unary, Multiplicative, Additive, Boolean, Comparison, Equality, Assign, Primary
+interface BinExpr {
+  type: Rule,
+  operator: string,
+  left: Expr,
+  right: Expr,
+}
+interface UnExpr {
+  type: Rule,
+  operator: string,
+  right: Expr,
 }
 
-
-export interface ParseError {
-  i: number,
-  message: string,
+interface Literal {
+  type: Rule,
+  kind: Type,
+  value: unknown,
 }
 
-export class Parser {
-  program: Expr[] = [];
-  expr!: Expr;
-  failed!: boolean;
-  errors!: ParseError[];
-  private lexer: Lexer;
-  constructor (source: string | Lexer) {
-    if (source instanceof Lexer) this.lexer = source;
-    else this.lexer = new Lexer(source);
-  }
-  eof () {
-    return !this.lexer.peekNext();
-  }
-  parse (): Expr[] {
-    if (this.eof()) {
-      const expr = this.parseAdditive<Expr>();
-      if (expr) {
-        this.expr = expr;
-        this.program.push(this.expr);
-        return this.parse();
-      }
-    }
-    return this.program;
-  }
-  static test (token: Token | null, type: Type, ...values: string[]) {
-    if (token == null) return false;
-    if ((token) && (token.type === type) && (typeof token.value == 'string') && (values.indexOf(token?.value + '') > -1))
-      return true;
-    else
-      return false;
-  }
-  parseExpr () {
-    return this.parseComparison();
-    // return this.parseAdditive();
-    // return this.parseEquality();
-  }
-  private parsePrimary () {
-    const token = this.lexer.peek();
-    switch (token?.type) {
-      case Type.PUNCT:
-        return this.parseGroup();
-      case Type.KW:
-        break
-      case Type.STR:
-      case Type.NUM:
-      case Type.SYM:
-        this.lexer.next();
-        return token;
-      default:
-        console.log(token?.toString());
-        console.error('error');
-    }
-  }
-  
-  private parseGroup () {
-    const expr = this.parseComparison();
-    this.skipNext(Type.PUNCT, ')');
-    // const ch = this.lexer.peek()?.value;
-    // if (ch !== ')') {
-    //   this.throwError("Expected ')', got " + ch);
-    // }
-    // this.lexer.next();
-    return expr;
-  }
-  // for sequences and lambda args
-  private collect (open: string, mid: string, close: string, parser: () => Expr): Expr[] {
-    const exprs: Expr[] = [];
-    // const peek = () => this.lexer.peek();
-    let init = true;
-    this.skipNext(Type.PUNCT, open);
-    while (!this.lexer.eof()) {
-      if (Parser.test(this.lexer.peek(), Type.PUNCT, close)) break;
-      if (init) init = false; else this.skipNext(Type.PUNCT, mid);
-      if (Parser.test(this.lexer.peek(), Type.PUNCT, close)) break;
-      exprs.push(parser());
-    }
-    this.skipNext(Type.PUNCT, close);
-    return exprs;
-  }
-  // expect (type: Type, ...values: string[]) {
-  //   const token = this.lexer.peek();
-  //   if (!Parser.test(token, type, ...values))
-  //     this.throwError(`Expected '${ values.join("', or '") }', but instead got token: ${ token }`);
-  //   else
-  //     this.lexer.next();
-  // }
-  private skipNext (type: Type, value: string) {
-    if (Parser.test(this.lexer.peek(), type, value)) this.lexer.next();
-    else
-      this.throwError(`Expecting token {type ${ Type[ type ] }, value ${ value }}, but got
-    ${ JSON.stringify(this.lexer.peek(), null, 2) }`);
-  }
-  private parseLambda() {}
-  private parseComparison<E> (): E {
-    if (this.eof())
-      this.throwError(`Unexpected end of input!`);
+type Expr = UnExpr | BinExpr | Literal | Token;
 
-    let expr = this.parseAdditive();
-    let tkn = this.lexer.peek();
-    while (tkn && expr && Parser.test(tkn, Type.OP, '<', '>', '<=', '>=')) {
-      this.lexer.next();
-      const rhs = this.parseAdditive();
-      expr = { type: Op.Comparison, operator: <string> tkn.value, left: expr, right: rhs };
-      tkn = this.lexer.peek();
-    }
-    return expr as E;
+class Parser {
+  lexer: Lexer;
+  constructor(source: string) {
+    this.lexer = new Lexer(source);
   }
-  private parseAdditive<E> (): E {
-    if (this.eof())
-      this.throwError(`Unexpected end of input!`);
-
-    let expr = this.parseMultiplicative();
-    let tkn = this.lexer.peek();
-    while (tkn && expr && Parser.test(tkn, Type.OP, '+', '-')) {
-      this.lexer.next();
-      const rhs = this.parseMultiplicative();
-      expr = { type: Op.Additive, operator: <string>tkn.value, left: expr, right: rhs };
-      tkn = this.lexer.peek();
-    }
-    return expr as E;
+  peek () { return this.lexer.peek(); }
+  next () { return this.lexer.next(); }
+  consume (literal: string) {
+    const ch = this.peek().literal;
+    if (ch !== literal)
+      this.lexer.error(`Expected the literal ${ literal } but instead got ${ ch }`);
+    this.next();
   }
-  private parseMultiplicative () {
-    if (this.eof())
-      return this.throwError(`Unexpected end of input!`);
-    
-    let expr = this.parsePrimary();
-    let tkn = this.lexer.peek();
-    while (tkn && expr && Parser.test(tkn, Type.OP, '*', '/')) {
-      this.lexer.next();
-      const rhs = this.parsePrimary();
-      expr = { type: Op.Multiplicative, operator: <string>tkn.value, left: expr, right: rhs };
-      tkn = this.lexer.peek();
+  atom (): Expr {
+    let token = this.peek();
+    if (token.type === Type.NUM || token.type === Type.STR) {
+      this.next();
+      return this.literal(token);
+    }
+    else if (token.validate(Type.PUNCT, '(')) {
+      this.next();
+      const expr: Expr = this.or();
+      this.consume(')');
+      return expr;
+    }
+    return token;
+  }
+  binary (parser: () => Expr, type: Rule, ...ops: string[]): Expr {
+    let expr: Expr = parser.call(this);
+    let token = this.peek();
+    while (token.validate(Type.OP, ...ops)) {
+      this.next();
+      const right = parser.call(this);
+      expr = { type, operator: token.literal, left: expr, right };
+      token = this.peek();
     }
     return expr;
   }
-
-  // TODO: debugging and errors
-  private logError (message: string) {
-    if (!this.failed) this.errors = [];
-    this.errors.push({ i: this.errors.length + 1, message});
+  or (): Expr { return this.binary(this.and, Rule.Or, '||'); }
+  and (): Expr { return this.binary(this.equality, Rule.And, '&&'); }
+  equality (): Expr { return this.binary(this.compare, Rule.Equality, '==', '!='); }
+  compare (): Expr { return this.binary(this.term, Rule.Compare, '<', '<=', '>', '>='); }
+  term (): Expr { return this.binary(this.factor, Rule.Term, '+', '-'); }
+  factor (): Expr { return this.binary(this.unary, Rule.Factor, '*', '/', '%'); }
+  unary (): Expr {
+    let token = this.peek();
+    if (token.validate(Type.OP, '!', '-')) {
+      this.next();
+      const right = this.unary();
+      return { type: Rule.Unary, operator: token.literal, right };
+    }
+    return this.atom();
   }
-  private throwError (message: string) {
-    // const { line, col } = this.lexer.pos;
-    const msg = message + `\n\tat (${ this.lexer.pos.line }:${ this.lexer.pos.col })`;
-    this.logError(msg);
-    console.log('Error log: ', this.errors);
-    this.lexer.error(msg);
+  literal (token: Token) {
+    return { type: Rule.Literal, kind: token.type, value: token.value };
   }
 }
 
