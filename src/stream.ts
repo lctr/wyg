25753543ae6a56d8df435c80934d1b91c82c2da7
Lictor Guzman <lctrgzmn@gmail.com>
2021-06-16 04:34:03 -0500

    toggled string normalization for now

--- src/stream.ts
+++ src/stream.ts
@@ -4,6 +4,9 @@ export class Stream {
   col = 0;
   end: number;
   constructor (private source: string) {
+    // to handle unicode chars with combining characters as 1 char if possible
+    // source = source.normalize();
+    this.source = source;
     this.end = source.length;
   }
   peek () {
@@ -15,13 +18,17 @@ export class Stream {
   next () {
     const char = this.source.charAt(this.pos++);
     if (char == '\n') this.col = 0, this.line++;
-    else { this.col++; }
+    else this.col++; 
     return char;
   }
   eof () {
-    return this.pos >= this.end;
+    return this.peek() == '';
   }
   error (msg: string) {
     throw new Error(`${ msg } at (${ this.line }:${ this.col })`);
+  }
+  // get line up to corrent position for logging
+  row () {
+    return this.source.slice(this.pos - this.col, this.col);
   }
 }
