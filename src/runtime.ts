3daee713cd8db96523a1dd2348aab858cc390ec8
Lictor Guzman <lctrgzmn@gmail.com>
2021-06-28 10:12:27 -0500

    global scope instance

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,11 +1,10 @@
-import { parse } from './parser.ts';
-import { evaluate } from './evaluator.ts';
-import { global } from './prelude.ts';
+import { parse } from "./parser.ts";
+import { evaluate, globalEnv } from "./environment.ts";
 
-export function run (program: string) {
+export function run(program: string) {
   try {
     const ast = parse(program);
-    return evaluate(ast, global);
+    return evaluate(ast, globalEnv);
   } catch (e) {
     return e;
   }
