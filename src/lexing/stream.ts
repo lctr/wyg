1b64244c432890a3cdb00eef3ff01965b9715054
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-10 00:13:23 -0500

    error handling

--- src/lexing/stream.ts
+++ src/lexing/stream.ts
@@ -1,9 +1,7 @@
-import { Colors } from "../printing.ts";
-
 export interface Streamable<T> {
-  peek (): T,
-  after?(): T,
-  next (): T,
+  peek (): T;
+  after?(): T;
+  next (): T;
   error (message: string): void;
 }
 
@@ -35,8 +33,7 @@ export class Stream implements Streamable<string> {
   error (msg: string) {
     // Stream.streamError(this, msg);
     const message = `${ msg } at (${ this.line }:${ this.col })`;
-    const snippet = `\n\n  [${ this.line }] · ${ this.source.slice(this.pos - this.col, this.col)
-      }\n`;
+    const snippet = `\n\n  [${ this.line }] · ${ this.source.slice(this.pos - this.col, this.col) }\n  ${ '-'.repeat((this.line + '').length + 4 + this.col) }^`;
 
     throw new Error(`${ message }${ snippet }`);
   }
@@ -44,24 +41,25 @@ export class Stream implements Streamable<string> {
   row () {
     return this.source.slice(this.pos - this.col, this.col);
   }
-  static streamError (stream: Stream, message: string) {
-    const indicator = [
-      " ".repeat(stream.col),
-      Colors.red("^"),
-    ].join("");
-    const row = stream.source.slice(0, stream.col);
-    const loc = `${ Colors.italic("at") } (${ Colors.brightBlue(stream.line + ":" + stream.col)
-      })`;
-    const gutter = (stream.line + "   ").split("").map((_) => " ").join("");
-    return [
-      message + ", instead got " + stream.peek(),
-      // gutter and vert divider
-      "",
-      // line number | source code slice
-      " " + stream.line + "|  " + row,
-      // arrow pointing to stream position of source code slice
-      gutter + " " + indicator,
-      " ".repeat((indicator).length) + Colors.gray("... ") + loc,
-    ].join("\n");
-  }
+  // static async streamError (stream: Stream, message: string) {
+  //   const { red, ii, blue0, gray } = await import("../printing.ts");
+
+  //   const indicator = [
+  //     " ".repeat(stream.col), red("^"),
+  //   ].join("");
+  //   const row = stream.source.slice(0, stream.col);
+  //   const loc = `${ ii("at") } (${ blue0(stream.line + ":" + stream.col)
+  //     })`;
+  //   const gutter = (stream.line + "   ").split("").map((_) => " ").join("");
+  //   return [
+  //     message + ", instead got " + stream.peek(),
+  //     // gutter and vert divider
+  //     "",
+  //     // line number | source code slice
+  //     " " + stream.line + "|  " + row,
+  //     // arrow pointing to stream position of source code slice
+  //     gutter + " " + indicator,
+  //     " ".repeat((indicator).length) + gray("... ") + loc,
+  //   ].join("\n");
+  // }
 }
